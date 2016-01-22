7f725fdab423a832ac89c3e2fe05eafa14483fd2
George Jia <george.jia@meginfo.com>
2016-01-22 17:58:15 +0800

    Init construct the home.html

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+$(function () {
+  console.log('ready!');
+  $('#settingIcon').on('click', function () {
+    $('#styleSetting').toggle();
+  });
+})
